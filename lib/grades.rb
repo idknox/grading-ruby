1a649a6d33ea0101794cc1063d9b5929eb79434b
Ian Knox <knoxid@gmail.com>
2014-09-17 09:31:31 -0600

    processing grades works

--- lib/grades.rb
+++ lib/grades.rb
@@ -4,6 +4,18 @@ class Grades
   end
 
   def process
-    @grades
+    output=[]
+    @grades.each_with_index do |grade, i|
+      unless i == 0
+       if grade > @grades[i-1]
+         output << :up
+       elsif grade < @grades[i-1]
+         output << :down
+       else
+         output << :even
+       end
+      end
+    end
+    output
   end
 end
--- spec/grades_spec.rb
+++ spec/grades_spec.rb
@@ -12,4 +12,13 @@ describe "Grades" do
 
     expect(processed_grades).to match_array([])
   end
+
+  it "processes grades" do
+    input = [6,3,5,4,3,4,4,5]
+    output = [:down,:up,:down,:down,:up,:even,:up]
+    grades = Grades.new(input)
+    processed_grades = grades.process
+
+    expect(processed_grades).to eq(output)
+  end
 end
